8d7afbaef70cc0af90f86c9ab0813ae3991e61c2
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-12-10 22:53:25 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Leftbar from "./components/leftbar/Leftbar";
+import Navbar from "./components/navbar/Navbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Update from "./components/update/Update";
+import "./app.css";
+import { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("jack");
+
+  return (
+    <>
+      <Navbar name={name} />
+      <div className="container">
+        <Leftbar />
+        <Update />
+        <Rightbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
