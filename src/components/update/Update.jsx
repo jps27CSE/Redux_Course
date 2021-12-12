db66d4cb87340984a689fa8bd1ee34eb630baea1
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-12-12 15:25:56 +0600

    dispatch

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -2,19 +2,36 @@ import React from "react";
 import Warning from "../warning/Warning";
 import "./update.css";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addHello, remove, update } from "../../redux/userSlice";
 
 export default function Update() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const user = useSelector((state) => state.user);
 
+  const dispatch = useDispatch();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+
+    dispatch(addHello({ name, email }));
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+
+    dispatch(remove());
+  };
+
   return (
     <div className="update">
       <div className="updateWrapper">
         <h3 className="updateTitle">Update Your Account</h3>
         <Warning />
-        <button className="delete">Delete Account</button>
+        <button className="delete" onClick={handleDelete}>
+          Delete Account
+        </button>
         <div className="updateContainer">
           <form>
             <div className="formItem">
@@ -50,7 +67,9 @@ export default function Update() {
               <label>Password</label>
               <input className="formInput" type="password" />
             </div>
-            <button className="updateButton">Update</button>
+            <button className="updateButton" onClick={handleUpdate}>
+              Update
+            </button>
           </form>
         </div>
       </div>
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -11,8 +11,12 @@ export const userSlice = createSlice({
       state.name = action.payload.name;
       state.email = action.payload.email;
     },
+    remove: (state) => (state = {}),
+    addHello: (state, action) => {
+      state.name = "Hello " + action.payload.name;
+    },
   },
 });
 
-export const { update } = userSlice.actions;
+export const { update, remove, addHello } = userSlice.actions;
 export default userSlice.reducer;
